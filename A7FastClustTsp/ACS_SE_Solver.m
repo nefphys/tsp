491d5ff719f957d5aeb40b52c0c97b1c97333c74
nefphys <xhy2012010258@live.com>
2022-08-12 18:47:05 +0800

    nothing

---
+++ A7FastClustTsp/ACS_SE_Solver.m
@@ -0,0 +1,180 @@
+function  [TSP_Solve_Struct]  =  ACS_SE_Solver(tspData, MaxIterations, startP, endP, target_length)
+
+
+    %% Ant Colony System  for  the TSP
+    %
+    %   ACSTSP(distances_matrix, number_of_ants, MaxIterations, alpha, beta, rho, target_length)
+    %
+    %   distances_matrix:   symmeric real (NrOfNodes x NrOfNodes) - matrix containing distances
+    %                       between all nodes: d[i,i]  =   0 , d[i,j] = d[j,i]
+    %   MaxIterations:      maximum number of iterations  to  run
+    %   target_length:       if  a tour  is  found  with  less than target_length, the  function  returns
+    %                       stops  and  returns this tour.
+    %
+    %   parameter standard values:
+    %       number_of_ants  =  NrOfNodes
+    %       alpha  =   1
+    %       beta  =   9
+    %       rho  =   0.9
+    %
+    % returns [BestTourLength, BestTour]
+
+
+    %% 初始化参数
+    [Distance, City] = readfile(tspData,1);
+    distances_matrix = Distance;
+    number_of_ants = int32(size(City,1)*0.8);
+    %MaxIterations = 1000;
+    %target_length = 100;
+
+    alpha  =   1; 
+    beta_0 = 1;
+    beta = beta_0;
+    beta_max = 7;
+    rho  =   0.1;
+    xi = 0.01;
+    xi_max = 0.5;
+
+    d  =  distances_matrix;
+    n  =  max(size(d));
+    m  =   number_of_ants ;
+    t_max  =   MaxIterations ;
+    L_target  =  target_length ;
+
+    L_best  =  inf;
+    T_best  =   0 ;
+
+    t1 = cputime;
+    %% 初始化信息素和路径
+    % INITIALIZATION  ===========================================================
+
+    
+    % pheromone trails
+    %c  =   1   /  (n  *  L_nn);
+    c  =   0.000001; %初始化信息素
+    tau  =  ones(n,n)  *  c;
+
+
+    % place m ants in n nodes
+    ant_tours  =  zeros(m, n + 1 ); %多了一个终点
+    %起点和终点是固定的
+    ant_tours(:, 1 )  =  startP; %每条路径在不同起点分配蚂蚁
+    ant_tours(:, end) = startP;
+    ant_tours(:,(end-1)) = endP;
+
+    %% 主循环
+    t  =   1 ;
+    kp = 1;
+    allrev = 0;
+    
+    %% 预分配内存
+    current_node = 0;
+    c_tv = 0;
+    r = 0;
+    select = 0;
+    city_to_visit = 0;
+    L_T  =  zeros( 1 ,m);
+    best_ant = 0;
+    
+    while  ((t  <=  t_max)  &&  (L_target  <=  L_best))
+
+
+        % CREATE TOURS  =============================================================
+
+        %% 生成新的路径以及局部信息素
+        [ant_tours, tau] = CalLocPh(m, ant_tours, n, alpha, beta,tau, rho, c, d);
+
+
+        % UPDATE  ===================================================================
+        %% 更新最短路径，更新参数
+           
+        %ant_tours(:,n + 1 )  =  ant_tours(:, 1 );
+        best_ant  =   1 ;
+        for  k  =   1  : m
+            P  =  ant_tours(k,:);
+            L_T(k)  =   sum(d(sub2ind(size(d),P(1:(end-1)),P(2:end))));
+            if  (L_T(k)  <  L_T(best_ant))
+                best_ant  =  k;
+            end
+        end
+        L_min  =  min(L_T);
+        T_min  =  ant_tours(best_ant,:);
+
+        %% 如果这次的路径和上次的路径一样，即都是best route，则更新beta和xi
+        if t >  5 && L_best <= L_min %5次之后再判断
+            if (1-0.95^kp) < xi_max
+                xi = 1-0.95^kp;
+            else
+                xi = xi_max;
+            end
+
+            if (beta_0 + log(kp)) <= beta_max
+                beta = beta_0 + log(kp);
+            else
+                beta = beta_max;
+            end
+
+            kp = kp + 1;
+        end
+
+        % update pheromone trails;
+        %% 更新全局信息素,只更新了最优的
+        for  i  =   1  : n
+            tau(T_min(i),T_min(i + 1 ))  =  ( 1   -  xi)  *  tau(T_min(i),T_min(i + 1 ))  +  xi  /  L_min;
+        end
+
+        % COMPLETE  ================================================================
+        t  =  t  +   1;
+        %current_cities  =  ant_tours(:,n);
+        ant_tours  =  zeros(m, n + 1 );
+        ant_tours(:, 1 )  =  startP; %每条路径在不同起点分配蚂蚁
+        ant_tours(:, end) = startP;
+        ant_tours(:,(end-1)) = endP;
+        if  (L_min  <  L_best)
+            L_best  =  L_min;
+            T_best  =  T_min;
+        end
+        allrev = [allrev, L_best];
+    end  % ends  while
+    t2 = cputime;
+    
+    TSP_Solve_Struct.time = t2-t1;
+    TSP_Solve_Struct.length = L_best;
+    TSP_Solve_Struct.route = T_best; %点下标从1开始
+    TSP_Solve_Struct.City = City;
+    TSP_Solve_Struct.BestLine = allrev;
+end
+
+function [ant_tours, tau] = CalLocPh(m, ant_tours, n, alpha, beta,tau, rho, c, d)
+    for  k  =   1  : m %蚂蚁
+        visited  =  ant_tours(k,:);
+        visited = visited(visited ~= 0);
+        to_visit  =  MY_setdiff(1:n,visited);
+        c_tv  =  length(to_visit);
+        p = zeros(1,c_tv);
+        for  s  =   2  : (n-1) %维数
+            current_node  =  ant_tours(k,s - 1 );
+            for LL = 1:c_tv
+                %计算局部信息素
+                p(LL) =  (tau(current_node,to_visit(LL))) ^ alpha  *  ( 1 / d(current_node,to_visit(LL))) ^ beta;
+            end
+            %p  =  (tau(current_node,to_visit)) .^ alpha  .*  ( 1 ./ d(current_node,to_visit)) .^ beta;
+            p  =  p  /  sum(p);
+            for  i  =   2  : c_tv
+                p(i)  =  p(i)  +  p(i - 1 );
+            end
+            r  =  rand; %这个就是q0 这里是每次都更新
+            select   =  to_visit(c_tv);
+            for  i  =   1  : c_tv
+                if  (r  <=  p(i))
+                    select   =  to_visit(i);
+                    break;
+                end
+            end
+            city_to_visit  =   select;
+            ant_tours(k,s)  =  city_to_visit;
+            %原始是1-rho * tau + tau0
+            tau(current_node,city_to_visit)  =  ( 1   -  rho)  *  tau(current_node,city_to_visit)  +  rho * c;
+        end
+    end
+end
--- A7FastClustTsp/FastClustTSP.m
+++ A7FastClustTsp/FastClustTSP.m
@@ -4,11 +4,14 @@ function [TSP_Solve_Struct] = FastClustTSP(tspData,MaxDistNum)
 % tspData 数据路径
 % MaxDistNum 最大可计算距离矩阵的大小 n*n, 数据量大于此值则不计算距离矩阵
 
+%算法内智能算法求解TSP最多可以计算999个点的情况
+
+
 %读取数据
 %判断数据量大小，如果数据量大于某个值，则不计算distance
 [Distance, City] = readfile(tspData,0);
 DataLen = size(City,1); %总数据大小
-ID = 1:DataLen;
+ID = 1:DataLen; %ID 与CITY对应的，方便后续查找数据
 if DataLen < 20000
     [Distance, City] = readfile(tspData,1);
 else
@@ -16,8 +19,78 @@ else
 end
 
 %% 初始化标准结构体
-std_struct = struct('inID',0,'outID',0,'set',0,'isover',0,'tsp',0,'order',0);
+std_struct = struct('inID',0,'outID',0,'set',0,'isover',0,'tsp',0,'order','0');
+ANS_GROUP = std_struct;
+ANS_GROUP.set = ID;
 
-
+%% 主循环,可以表示层数，默认最多999层和999个数据
+layer = 1; 
+while(true)
+    % 对结构体中的每个数据进行判断是否计算
+    % 双倍内存的形式，生成一个伴生的空结构体
+    ANS_GROUP_FAKE = [];
+    %是否还需要计算
+    isCal = 0;
+    for i = 1:length(ANS_GROUP)
+        tarStruct = ANS_GROUP(i);
+        tempStruct = std_struct;
+        %isover==0则需要继续计算，否则返回原来的结构体
+        %计算之后返回的也是一个结构体数组, 如果有变化则删除原本的结构体，并拼接新的
+        if tarStruct.isover == 0
+            isCal = 1;
+            %判断集合内点的数量，以确定是聚类还是计算TSP
+            setSize = length(tarStruct.set);
+            
+            % 根据入点和出点数据判断当前结构体是不是第一次进入计算
+            if tarStruct.in == 0
+                %是第一次进入计算，再判断是否进行聚类
+                
+                %如果是单点集，则返回当前点
+                if setSize == 1
+                    tempStruct.in = tarStruct.set;
+                    tempStruct.out = tarStruct.set;
+                    tempStruct.set = tarStruct.set;
+                    tempStruct.isover = 1;
+                    tempStruct.tsp = tarStruct.set;
+                    tempStruct.order = '0'; %原始点就是单点集，没有计算的必要
+                %如果只有两个点，则按顺序返回
+                elseif setSize == 2
+                    tempStruct.in = tarStruct.set(1);
+                    tempStruct.out = tarStruct.set(2);
+                    tempStruct.set = tarStruct.set;
+                    tempStruct.isover = 1;
+                    tempStruct.tsp = tarStruct.set;
+                    tempStruct.order = '0'; %原始点就是单点集，没有计算的必要
+                %如果点的数量小于200，则计算TSP
+                elseif setSize <= 200
+                    tempStruct.in = 0;
+                    tempStruct.out = 0;
+                    tempStruct.set = tarStruct.set;
+                    tempStruct.isover = 1;
+                    tempStruct.tsp = TSP_Solver(City(tarStruct.set,:)); %调用求解器
+                    tempStruct.order = '0';
+                %如果点的数量大于200，但是小于10000，则计算密度聚类
+                elseif setSize <= 10000
+                
+                %如果点的数量大于10000，则进行kmeans聚类
+                else
+                    
+                end
+                
+            else
+                %不是第一次进入计算了，也判断是否进行聚类
+            end
+        else
+            ANS_GROUP_FAKE = [ANS_GROUP_FAKE tarStruct];
+        end
+    end
+    
+    %终止条件，只要后面一次循环isover全部是1，即借宿
+     if isCal == 0
+         break;
+     end
+     %层数增加
+    layer = layer + 1;
+end
 
 end
